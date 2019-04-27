d3f3191d69039cb314c760e734e91e6a91da5935
Jordan Jacobelli <jjacobelli@nvidia.com>
2019-04-26 17:03:25 -0700

    Loop until we can initialize NVML
    Doing this, we are waiting for the node to be configured as a GPU node
    instead of crashing.
    
    Signed-off-by: Jordan Jacobelli <jjacobelli@nvidia.com>

--- main.go
+++ main.go
@@ -57,13 +57,12 @@ func main() {
 
 func run(nvmlInterface NvmlInterface, conf Conf) error {
 
-	if err := nvmlInterface.Init(); err != nil {
-		// TODO: Update README and links
-		log.Printf("Failed to initialize NVML: %s.", err)
+	for err := nvmlInterface.Init(); err != nil; time.Sleep(time.Minute) {
+		log.Printf("Failed to initialize NVML: %v.", err)
 		log.Printf("If this is a GPU node, did you set the docker default runtime to `nvidia`?")
-		log.Printf("You can check the prerequisites at: https://github.com/NVIDIA/gpu-feature-discovery")
+		log.Printf("You can check the prerequisites at: https://github.com/NVIDIA/gpu-feature-discovery#prerequisites")
 		log.Printf("You can learn how to set the runtime at: https://github.com/NVIDIA/gpu-feature-discovery#quick-start")
-		return err
+		log.Printf("Will try again in 60s")
 	}
 
 	defer func() {
